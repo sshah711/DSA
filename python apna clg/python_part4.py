f294b0d038148d9398f8b11ae0281ceffddee4b0
sshah711 <sshah380008@gmail.com>
2026-08-12 11:15:11 +0530

    update Python part 4 with encapsulation, inheritance, abstraction, and polymorphism examples

--- python apna clg/python_part4.py
+++ python apna clg/python_part4.py
@@ -158,3 +158,145 @@ products.get_count()
 p1.cal_discountt(p1.price, 10)
 
 
+# encapsulation- data hiding & binding - methods and attr are bind together  
+# attr- public-access claas inside or outside , private- access only inside the class, protected- access inside the class as well as sub class(inheritance)
+
+class socialmedia:
+    def __init__(self, name, followers, platform,reels):
+        self.name=name #by default public
+        self.followers=followers
+        self._platform=platform #atr ke aage _ lga diya so now its become protected attr
+        self.__reels=reels #atr ke aage __ lga diya so now its become private attr 
+        # data mangling
+
+    def get_reels(self):
+        return self.__reels
+    def set_reels(self,newreel):
+            self.__reels=newreel
+
+soc1=socialmedia("sakshi",350,"instagram",22)
+soc1.set_reels(11)
+print(soc1.name,soc1.followers,soc1._platform,soc1.get_reels())
+print(soc1.name,soc1._platform,soc1._socialmedia__reels)  #objname._classname__private attr --- is tarah se b hum private attr ko access kr skte h
+
+
+# inheritance - ek class ki property ko dusre class me use krna
+# reusing atrr and methods from parent class(base class) to child class(derived class)
+# 3type of inheritance are there: 1) single level I- one parent child 
+# 2)multilevel I- parent ke niche child uske b niche ek or child - example: employee->adminstaff->account
+# 3)multiple I- multiple parent ki property child me hogi - example: teacher, student are parent & TA are chlid so that IA class ke pass thodi student class ki property hogi or thodi teacher class ki 
+
+
+class employess:   #patent class
+    # def __init__(self,stime,etime):
+        # self.name=name
+        stime="10am"
+        etime="5pm"
+
+        def change_time(self,new_etime):
+            self.etime=new_etime
+
+    # [ multilevel I
+class teacher(employess):     #child class
+    def __init__(self,sub):
+        self.sub=sub
+
+class admin(employess):          #child class
+    def __init__(self,role):
+        self.role=role
+
+class account(admin):
+    def __init__(self, salary,role):
+        super().__init__(role)
+        self.salary=salary
+# ]
+
+
+# [ multiple I
+class teachers():     #parent class
+    def __init__(self,sub):
+        self.sub=sub
+
+class student():            #parent class
+    def __init__(self,year):
+        self.year=year
+
+class TA(teachers,student):          #child class
+    def __init__(self,sub,year,name):
+        super().__init__(sub)
+        student.__init__(self,year)
+        self.name=name
+
+# ]
+
+# e1=employess("sakshi","10am","6pm")
+ta1=TA("physics",2020,"shah sakshi")
+print(ta1.name,ta1.year,ta1.sub)
+t1=teacher("math")
+t2=admin("manager")
+t3=account(23000,"hod")
+t1.change_time("2pm")
+t2.change_time("3pm")
+t3.change_time("9pm")
+print(t1.sub,t1.stime,t1.etime)
+print(t2.role,t2.stime,t2.etime)
+print(t3.salary,t3.role,t3.etime)
+
+
+# abstraction - hinding internal details and show only essential details - example: chatgpt ya claude h to usme hum direct answer dekh pate h backend me konsa algo run ho rha h ya konsa data akha ja rha h we don't know
+# diff btn encalsulation data hiding and abstraction is that wha pr hum data ko hide krte h sirf or yha pr humko kon se data ko hide krna h as well as konse data ko show krna h wo dikhate h
+# abstract class - blueprint for other classes
+# not implement the method of abstract class but inhe hum inherit kr ke dusre(child) class me implement krte h
+from abc import ABC, abstractmethod
+
+class animal(ABC):
+    @abstractmethod
+    def makesound(self):     #implementiation details ko hide kr rhe h
+        pass
+
+
+class cat(animal):
+    def makesound(self):
+        print("meow")
+
+
+class cow(animal):
+    def makesound(self):
+        print("moo")
+
+c1=cat()
+c1.makesound()
+c2=cow()
+c2.makesound()
+
+
+# polymorphism- many forms - multiple functions with same name & diff implementation
+# operator overloading- ex: + add, concate
+print(1+2, "h"+"ii")
+# function overriding (inheritance)- redefining parent class's fuc in child class-- parent - fun() same child- fun()
+# duck typing-- walks like a duck & quacks like a duck 
+
+class Emp:
+    def get_deg(self):
+        print("deg is emp")
+class Teacher(Emp):
+    def get_deg(self):   #override the same function 
+        print("deg is teacher")
+
+t=Teacher()
+t=Emp()
+t.get_deg()
+
+# dono class ek dusre se relavent nhi h koi kisi ki subclass nhi h lekin dono ke andar same kam krne vala function h to ise hum duck typing kehte h
+class Teacher1():
+    def get_deg(self):   #duck typing the same function 
+        print("deg is tea")
+
+class Accountant():
+    def get_deg(self):   #duck typing the same function 
+        print("deg is actnt")
+
+t=Teacher1()
+t.get_deg()
+t=Accountant()
+t.get_deg()
