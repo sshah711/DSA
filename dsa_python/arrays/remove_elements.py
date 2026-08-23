fe1635b7fa1398cccd7a8dbdb5ed4df632cb2eea
sshah711 <sshah380008@gmail.com>
2026-08-24 01:26:43 +0530

    change

--- dsa_python/arrays/remove_elements.py
+++ dsa_python/arrays/remove_elements.py
@@ -10,11 +10,9 @@ def remove_element(a, n):
 
     # total unique element nums, modified array
 
-
 n = 6
-aa = [0,6, 1, 1, 6,2, 2, 4, 4, 6, 6]
+aa = [0, 6, 1, 1, 6, 2, 2, 4, 4, 6, 6]
 print(remove_element(aa, n))
-
 
 # extra-works only sorted array are there
 def remove_duplicate_element_also_remove_element_that_mentioned(a, n):
@@ -32,12 +30,12 @@ def remove_duplicate_element_also_remove_element_that_mentioned(a, n):
 
 
 n = 6
-aa = [0, 0, 1,2,3,3,4,6,6,7,8]
+aa = [0, 0, 1, 2, 3, 3, 4, 6, 6, 7, 8]
 
 print(remove_duplicate_element_also_remove_element_that_mentioned(aa, n))
 
 
-# if there is unsorted array
+# if there is unsorted array then use set
 def remove_duplicate(a, n):
     p1 = 0
     seen = set()
@@ -52,7 +50,6 @@ def remove_duplicate(a, n):
             p1 += 1
 
     return p1, a[:p1]
-
 
 n = 6
 aa = [0, 0, 6, 1, 2, 5, 9, 72, 3, 4, 4, 6, 1, 2, 6]
