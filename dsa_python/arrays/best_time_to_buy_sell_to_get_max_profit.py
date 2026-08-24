af60b57f28905acd663007abc4c2c2cf03fd5c6c
sshah711 <sshah380008@gmail.com>
2026-08-24 10:56:38 +0530

    Add Best Time to Buy and Sell Stock solution in Python

---
+++ dsa_python/arrays/best_time_to_buy_sell_to_get_max_profit.py
@@ -0,0 +1,24 @@
+def best_time_to_buy_sell_to_get_max_profit(price):
+    profit = 0
+    min = price[0]
+    ans = 0
+    n = len(price)
+    # brute force approach O(n^2)
+    # for i in range(n):
+    #     for j in range(i, n):
+    #         if price[i] < price[j]:
+    #             ans = price[j] - price[i]
+    #             if profit < ans:
+    #                 profit = ans
+    # return profit
+
+    for i in range(n):
+        if price[i] - min > profit:
+            profit = price[i] - min
+        if price[i] < min:
+            min = price[i]
+    return profit
+
+
+p = [7, 5, 3, 4]
+print(best_time_to_buy_sell_to_get_max_profit(p))
