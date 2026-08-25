7c6f65bce13c1cb0e33b3a193991e9ac633bd4c7
sshah711 <sshah380008@gmail.com>
2026-08-25 09:48:39 +0530

    changes

--- dsa_python/arrays/missing-number.py
+++ dsa_python/arrays/missing-number.py
@@ -1,13 +1,12 @@
 def missing_number(a):
-    n=len(a)
-    sumt=n*(n+1)//2
-    suma=0
+    n = len(a)
+    sumt = n * (n + 1) // 2
+    suma = 0
     for i in range(n):
-            
-        suma= suma+a[i]
 
-        
-    return sumt-suma
+        suma = suma + a[i]
 
-a=[1,3,2,5,0]
+    return sumt - suma
+
+a = [1, 3, 2, 5, 0]
 print(missing_number(a))
