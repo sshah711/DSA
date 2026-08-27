7f1514ff38db27013cf79c09155cd24f412410c8
sshah711 <sshah380008@gmail.com>
2026-08-28 00:12:54 +0530

    exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.

--- dsa_python/arrays/single_number.py
+++ dsa_python/arrays/single_number.py
@@ -23,3 +23,30 @@ def singleNumber(nums):
         return x
 n = [1, 1,1, 2,3,2,2]
 print(singleNumber(n))
+
+
+#  exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+def singleNumber(nums):
+        xor_all = 0
+
+        # Step 1: XOR all numbers
+        for num in nums:
+            xor_all ^= num
+
+        # Step 2: Find rightmost set bit
+        rightmost_bit = xor_all & -xor_all
+
+        x = 0
+        y = 0
+
+        # Step 3: Divide into two groups and XOR
+        for num in nums:
+            if num & rightmost_bit:
+                x ^= num
+            else:
+                y ^= num
+
+        return [x, y]
+
+n = [1, 1,2,2,8,5]
+print(singleNumber(n))
