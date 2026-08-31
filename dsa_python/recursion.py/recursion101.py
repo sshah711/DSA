98a4553588db3df05c7e98e3a4d01c59faf8ac9e
sshah711 <sshah380008@gmail.com>
2026-08-31 08:12:05 +0530

    Update recursion practice examples with base cases and step iteration

--- dsa_python/recursion.py/recursion101.py
+++ dsa_python/recursion.py/recursion101.py
@@ -1,10 +1,23 @@
 def recursion(n):
-
+    if n<1:
+        return
+    print(n)
+    n=n-1
     recursion(n)
 
+n=8
+(recursion(n))
 
 
+def recur(x):
+    if x>n:
+        return
+    # for n in range(n):
+    print(x)
+    x=x+2
+    recur(x)
 
 
+# n=8
 
-print(recursion(n))
+(recur(1))
