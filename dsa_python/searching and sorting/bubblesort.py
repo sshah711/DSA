d6081ba856c898ddb776b6041cf583471ade4e87
sshah711 <sshah380008@gmail.com>
2026-09-01 09:38:44 +0530

    Add Selection Sort implementation and refine Bubble Sort logic in Python

--- dsa_python/searching and sorting/bubblesort.py
+++ dsa_python/searching and sorting/bubblesort.py
@@ -1,17 +1,17 @@
+# after the first iteration largest number is bubble up at the end of arr
+
 def bubblesort(arr):
     for i in range(len(arr) - 1):
-        isSwapped=False
+        isSwapped = False  # check for is swapped or not bcoz if the array is already sorted at that time also this whole loop runs, so avoiding these we can make boolean function that checks wethear swapped or not
         for j in range(len(arr) - 1 - i):
             if arr[j] > arr[j + 1]:
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
-                isSwapped=True
-        if(not isSwapped):
-           break
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                isSwapped = True
+        if not isSwapped:
+            break
     return arr
 
 
 arr = [6, 4, 8, 2, 5, 1]
-arr = [1,2,3]
+arr = [1, 2, 3]
 print(bubblesort(arr))
---
+++ dsa_python/searching and sorting/selectionsort.py
@@ -0,0 +1,14 @@
+# select the minimum element and move this min element to right position
+def selectionsort(arr):
+    for i in range(len(arr) - 1):
+        min = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min]:
+                min = j
+        if min != i:
+            arr[i], arr[min] = arr[min], arr[i]
+    return arr
+
+
+arr = [2, -5, 1, 6, 8, 3]
+print(selectionsort(arr))
