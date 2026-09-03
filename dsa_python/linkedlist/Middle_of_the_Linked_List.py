221879e21fb0d56fd891977b697bd8da52790eb5
sshah711 <sshah380008@gmail.com>
2026-09-03 08:52:21 +0530

    Add Middle of the Linked List solution using slow and fast pointers in Python

---
+++ dsa_python/linkedlist/Middle_of_the_Linked_List.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def Middle_of_the_Linked_List(head):
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow
+
+# Creating Linked List:
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+head.next.next.next.next.next = Node(50)
+
+middle = Middle_of_the_Linked_List(head)
+
+print(middle.val)
