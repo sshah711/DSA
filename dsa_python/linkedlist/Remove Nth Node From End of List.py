cf53b6489591394c330fe7cc4406c4ac4ec53eba
sshah711 <sshah380008@gmail.com>
2026-09-08 17:13:26 +0530

    Add single-pass two-pointer solution for Remove Nth Node From End of List in Python

--- dsa_python/linkedlist/Remove Nth Node From End of List.py
+++ dsa_python/linkedlist/Remove Nth Node From End of List.py
@@ -1,3 +1,6 @@
+from unittest.mock import sentinel
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -21,12 +24,32 @@ def Remove_Nth_Node_From_End_of_List(head, n):
     prev.next = prev.next.next
     return sentinel.next
 
+    # 1pass solution - using two pointers, first pointer is moved n steps ahead and then both pointers are moved together until the first pointer reaches the end of the list. The second pointer will be at the node before the nth node from the end of the list.
+
+def Remove_Nth_Node_From_End_of_List1(head, n):
+    sentinel = ListNode()
+    sentinel.next = head
+    first = second = sentinel
+    # first pointer is moved n steps ahead
+    for _ in range(n):
+        first = first.next
+
+    # both pointers are moved together until the first pointer reaches the end of the list
+    while first.next:
+        first = first.next
+        second = second.next
+        
+    second.next = second.next.next
+    return sentinel.next
+
+
 def print_list(head):
     curr = head
     while curr:
         print(curr.val, end=" -> " if curr.next else "")
         curr = curr.next
     print()
+
 
 head = ListNode(1)
 head.next = ListNode(2)
@@ -36,5 +59,6 @@ head.next.next.next.next = ListNode(9)
 head.next.next.next.next.next = ListNode(8)
 
 print_list(head)
-head = Remove_Nth_Node_From_End_of_List(head, 2)
+# head = Remove_Nth_Node_From_End_of_List(head, 2)
+head = Remove_Nth_Node_From_End_of_List1(head, 1)
 print_list(head)
